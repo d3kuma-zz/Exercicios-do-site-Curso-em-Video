93a604de44e0a7d773bd0284e00e16254fcb2c28
André Kumagae <andre.kumagae@hotmail.com>
2021-03-31 11:06:11 -0300

    Ex 14 hora do dia

--- Curso JS/aula12ex/ex14/script.js
+++ Curso JS/aula12ex/ex14/script.js
@@ -1,1 +1,22 @@
-alert('Olá')
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var saudacao = document.getElementById('saudacao')
+    var data = new Date()
+    // Método que pega a hora do sistema
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora>=0 && hora < 12){
+        img.src = 'manha.png'
+        saudacao.innerHTML = ` Bom dia!`
+        document.body.style.background = '#C2B187'
+    }else if (hora >= 12 && hora <= 18){
+        img.src = 'tarde.png'
+        saudacao.innerHTML = ` Boa tarde!`
+        document.body.style.background = '#b9846f'
+    }else{
+        img.src = 'noite.png'
+        saudacao.innerHTML = ` Boa noite!`
+        document.body.style.background = '#515154'
+    }
+}
