6075a807fedd8b80920dcf6e4a0539eddb970fb2
André Kumagae <andre.kumagae@hotmail.com>
2021-03-30 21:39:57 -0300

    Exercicios if e switch

---
+++ Curso JS/aula12/ex012.js
@@ -0,0 +1,11 @@
+// Comando para pegar a hora atual do sistema
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`);
+if (hora < 12){
+    console.log('Bom dia!');
+}else if (hora <= 18){
+    console.log('Boa tarde!');
+}else {
+    console.log('Boa noite!');
+}
