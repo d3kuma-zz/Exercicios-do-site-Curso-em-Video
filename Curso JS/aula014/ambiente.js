8bb94befeaacc08acaf6248a8ca1390c8ccbb165
André Kumagae <andre.kumagae@hotmail.com>
2021-04-01 16:15:58 -0300

    Exercicio For Vamos Contar

--- Curso JS/aula014/ambiente.js
+++ Curso JS/aula014/ambiente.js
@@ -1,16 +1,5 @@
-/* var c = 1
-while (c <= 6){
-    console.log(`Passo ${c}`)
-    c++
-} */
-
-/* do{
-    console.log(`Passo ${c}`);
-    c++
-}while (c<=6) */
-
-// console.log('Vai começar...');
-// for (var c = 1; c <= 4; c++){
-//     console.log(c);
-// }
+console.log('Vai começar...');
+for (var c = 4; c >= 1; c--){
+    console.log(c);
+}
 console.log('FIM!');
--- Curso JS/aula014ex/modelo/script.js
+++ Curso JS/aula014ex/modelo/script.js
@@ -1,17 +1,37 @@
 function contar() {
-    var i = document.getElementById('txtini')
-    var f = document.getElementById('txtfim')
-    var p = document.getElementById('txtpas')
-    var res = document.querySelector('div#msg')
-    res.innerHTML = `<p>Contando...</p>`
-    if (Number(p.value) == 0) {
-        alert('[ERRO]: Passo não pode estar vazio.')
-    } else {
+    // Variáveis que pegam os valores das caixas
+    let ini = document.getElementById('txtini')
+    let fim = document.getElementById('txtfim')
+    let passo = document.getElementById('txtpas')
+    let res = document.querySelector('div#msg')
 
-        for (var c = Number(txtini.value); c <= Number(txtfim.value); c--) {
-            res.innerHTML += `&#x1F449; ${c} `
-            
+    // Validação se as caixas de texto estão vazias
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar! Faltam dados!'
+    } else {
+        res.innerHTML = `Contando... <br>`
+        // Conversão dos primitivos de texto para inteiro
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        // Validação se o passo não foi inserido valor 0 ou menor
+        if (p <= 0){
+            alert('PASSO inválido! Configurado para PASSO = 1')
+            p = 1
         }
-        res.innerHTML += `&#x1F6A9;`
+        // FOR em ordem crescente
+        if (i < f){
+            for (c = i; c <= f; c += p){
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        }
+        // FOR em ordem decrescente
+        else{
+            for (c = i; c >= f ; c -= p){
+                res.innerHTML += `${c} \u{1F449} `
+            }
+        }
+        // Emoji final. Perceba que funciona tanto com '\u{}' quanto '&#x'
+        res.innerHTML += ` &#x1f6A9`
     }
 }
